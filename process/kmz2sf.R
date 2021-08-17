76ea4e8a87d38c540d383d3ef324fd8875b9ddcf
Ariel <ariel.fuentesdi@usach.cl>
2021-08-17 17:49:38 -0400

    intro

---
+++ process/kmz2sf.R
@@ -0,0 +1,23 @@
+kmz2sf <- function(kmz_path, path, exdir){
+  library(fs)
+  library(purrr)
+  library(dplyr)
+  library(sf)
+  #unzip
+  target <- paste0(path, ".temp.kml.zip")
+  file_copy(kmz_path, target, overwrite = T)
+  unzip(target, exdir = exdir)
+  #read kml ----
+  ##kml layers ----
+  kml_dir <- paste0(path, "doc.kml")
+  kml_layers <- st_layers(kml_dir)
+  kml_fl <- kml_layers$name %>%
+    map(function(x) read_sf(kml_dir, layer = x)) %>%
+    bind_rows()
+  return(kml_fl)
+}
+
+# kmz2sf(kmz_path = "data/trazado+concurso+valparaíso.kmz", 
+#        path = "data/", 
+#        exdir = paste0(getwd(), "/data"))  
+
--- process/main.R
+++ process/main.R
@@ -1,5 +1,15 @@
 #libraries ----
 library(rmarkdown)
+library(dplyr)
+source("process/kmz2sf.R", encoding = "utf-8")
+#read kmz ----
+elec_rt <- kmz2sf(kmz_path = "data/trazado+concurso+valparaíso.kmz",
+                  path = "data/",
+                  exdir = paste0(getwd(), "/data")) %>%
+  select(-Description) %>%
+  rename(Rutas = Name) %>%
+  mutate(Rutas = c("E01I", "E01R", "E02NR", "E02I", "E02R"),
+         Dist = units::set_units(st_length(.), "km"))
 #calling process ----
 render(input = "report/reportProcess.Rmd", 
        output_file = "Reporte Demanda", 
