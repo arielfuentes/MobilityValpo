da8c24ba8322c68fd5e9a68f4fc47e0670a8a20f
Ariel <ariel.fuentesdi@usach.cl>
2022-03-10 17:50:23 -0300

    new times

--- process/newdata_quilpue.R
+++ process/newdata_quilpue.R
@@ -1,8 +1,20 @@
 library(readr)
-filter(trips, 
+tviaje_serday <- filter(trips, 
        Servicio %in% c("104", "115", "120", "121", "122", "123", "125")) %>%
   mutate(TipoDia = factor(TipoDia, c("laboral", "sábado", "domingo"))) %>%
   group_by(TipoDia, Servicio, Sentido) %>%
-  summarise(tviaje = mean(tviaje))
+  summarise(tviaje = mean(tviaje)) %>%
+  ungroup()
 
-new_times <-  read_delim("data/tviaje_quilpue.csv", delim = ";") 
+new_times <-  read_delim("data/tviaje_quilpue.csv", delim = ";") %>%
+  rename(tviaje_new = tviaje)
+
+delta_times <- left_join(tviaje_serday, new_times) %>%
+  mutate(delta = tviaje_new/tviaje) %>%
+  select(-c("tviaje", "tviaje_new"))
+
+trips_new <- 
+  trips %>%
+  left_join(delta_times) %>%
+  mutate(tviaje = if_else(!is.na(delta), tviaje*delta, tviaje)) %>%
+  select(-delta)
